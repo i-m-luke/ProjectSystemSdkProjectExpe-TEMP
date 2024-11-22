94c2a2a0cb25f0f8c2e572377c0f70fb5bcb30c9
Lukáš Kratochvíl <kratochvil.luke@gmail.com>
2024-11-22 15:35:48 +0100

    Add vs.extensibility support (compilation is now working)

---
+++ SystemTestToolkit.Extension/VsExtensibilityEntrypoint.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Utilities;
+
+using System.Threading.Tasks;
+using System.Threading;
+
+using EnvDTE;
+
+using CommunityToolkit.Mvvm.Messaging;
+
+using Microsoft.VisualStudio.Extensibility.Shell;
+
+
+namespace SystemTestToolkit.Extension
+{
+    /// <summary>
+    /// Extension entrypoint for the VisualStudio.Extensibility extension.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class VsExtensibilityEntrypoint : Microsoft.VisualStudio.Extensibility.Extension
+    {
+        /// <inheritdoc />
+        public override ExtensionConfiguration ExtensionConfiguration => new()
+        {
+            RequiresInProcessHosting = true,
+            LoadedWhen = ActivationConstraint.ActiveProjectCapability(
+                ProjectCapability.Custom(Constants.PojectCapabilities.SystemTestPackage))
+        };
+
+        /// <inheritdoc />
+        protected override void InitializeServices(IServiceCollection serviceCollection)
+        {
+            base.InitializeServices(serviceCollection);
+
+            // You can configure dependency injection here by adding services to the serviceCollection.
+        }
+
+        protected override async Task OnInitializedAsync(VisualStudioExtensibility extensibility, CancellationToken cancellationToken)
+        {
+            await base.OnInitializedAsync(extensibility, cancellationToken);
+        }
+    }
+}
