62221bf2d3e8291d2db06287cf8747825b74e1b6
Lukáš Kratochvíl <kratochvil.luke@gmail.com>
2024-12-03 12:15:22 +0100

    Add changes ...

---
+++ SystemTestToolkit.Extension/README.md
@@ -0,0 +1,3 @@
+﻿Vyzkoušeno:
+- Rozšíření definic rules položek projektu (Build/Rules/ProjectItemsSchema.xaml)
+- Přidání různých project capabilities: viz UnconfiguredPackageProject.cs
--- SystemTestToolkit.Extension/VSSDK/ProjectSystem/UnconfiguredPackageProject.cs
+++ SystemTestToolkit.Extension/VSSDK/ProjectSystem/UnconfiguredPackageProject.cs
@@ -25,26 +25,33 @@ namespace SystemTestToolkit.Extension.VSSDK.ProjectSystem
 
         public const string ProjectLanguage = Constants.ProjectCapabilities.SystemTestPackage;
 
+        // NOTE:
+        // When non-SDK project is loaded the project has these capabilites:
+        // .NET;AllTargetOutputGroups;AppSettings;AssemblyReferences;ClassDesigner;COMReferences;ConfigurableFileNesting;CPS;CSharp;DataSourceWindow;DeclaredSourceItems;DependenciesTree;DiagnoseCapabilities;DisableBuiltinDebuggerServices;DynamicDependentFile;FallbackProjectConfigurationService;FolderPublish;HostSetActiveProjectConfiguration;IntegratedConsoleDebugging;LaunchProfiles;Managed;Microsoft.VisualStudio.ProjectSystem.RetailRules;NoGeneralDependentFileIcon;OutputGroups;PersistDesignTimeDataOutOfProject;ProjectImportsTree;ProjectPropertiesEditor;ProjectPropertyInterception;ProjectReferences;Publish;RelativePathDerivedDefaultNamespace;RunningInVisualStudio;SharedProjectReferences;SingleFileGenerators;SupportAvailableItemName;SystemTestPackage;UseFileGlobs;UserSourceItems;VisualStudioWellKnownOutputGroups;WinRTReferences
+        // Problematic A : HostSetActiveProjectConfiguration;IntegratedConsoleDebugging;LaunchProfiles;Managed;
         public const string Capabilities =
 #if DEBUG
             ProjectSystemProjectCapabilities.DiagnoseCapabilities + ";" + // Support for diagnosing project capabilities
 #endif
-            Constants.ProjectCapabilities.SystemTestPackage + ";" + // Custom capability for the project type
-            ProjectSystemProjectCapabilities.Cps + ";" +
-            ProjectSystemProjectCapabilities.CSharp + ";" +
-            ProjectSystemProjectCapabilities.ProjectReferences + ";" +
-            ProjectSystemProjectCapabilities.PreserveFormatting + ";" +
-            ProjectSystemProjectCapabilities.HandlesOwnReload + ";" +
-            ProjectSystemProjectCapabilities.ProjectConfigurationsDeclaredDimensions + ";" +
-            ProjectSystemProjectCapabilities.SharedImports + ";" +
-            ProjectSystemProjectCapabilities.UseProjectEvaluationCache + ";" +
-            ProjectSystemProjectCapabilities.ShowMissingItemTypes + ";" +
-            "DependenciesTree;" + // Support for displaying dependencies tree in Solution Explorer
-            "Pack;" + // Support for nuget pack
-            "DotNetCore;" + // Support for .NET Core projects
-            "AppDesigner;" + // Indicates that the project uses the app designer for managing project properties
-            "EditAndContinue;" + // Indicates that the project supports the edit and continue debugging feature
-            "UserSourceItems;"; // Indicates that the user is allowed to add arbitrary files to their project
+            Constants.ProjectCapabilities.SystemTestPackage + ";";
+        //Constants.ProjectCapabilities.SystemTestPackage + ";" + // Custom capability for the project type
+        //ProjectSystemProjectCapabilities.Cps + ";" +
+        //ProjectSystemProjectCapabilities.CSharp + ";" +
+        //ProjectSystemProjectCapabilities.ProjectReferences + ";" +
+        //ProjectSystemProjectCapabilities.PreserveFormatting + ";" +
+        //ProjectSystemProjectCapabilities.HandlesOwnReload + ";" +
+        //ProjectSystemProjectCapabilities.ProjectConfigurationsDeclaredDimensions + ";" +
+        //ProjectSystemProjectCapabilities.SharedImports + ";" +
+        //ProjectSystemProjectCapabilities.UseProjectEvaluationCache + ";" +
+        //ProjectSystemProjectCapabilities.ShowMissingItemTypes + ";" +
+        //"DependenciesTree;" + // Support for displaying dependencies tree in Solution Explorer
+        //"Pack;" + // Support for nuget pack
+        //".NET;" + // Indicates that the project for .NET 
+        //"DotNetCore;" + // Indicates that the project .NET Core projects
+        //"AppDesigner;" + // Indicates that the project uses the app designer for managing project properties
+        //"EditAndContinue;" + // Indicates that the project supports the edit and continue debugging feature
+        //"UserSourceItems;"; // Indicates that the user is allowed to add arbitrary files to their project
+
 
         [ImportingConstructor]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "ConvertToPrimaryConstructor")]
