68fedd5006743175ee03d04ccd1c11fcc12a4741
Lukáš Kratochvíl <kratochvil.luke@gmail.com>
2025-07-09 15:02:34 +0200

    ...

--- SystemTestToolkit.Extension/VSSDK/ProjectSystem/ProjectTypeRegistration.cs
+++ SystemTestToolkit.Extension/VSSDK/ProjectSystem/ProjectTypeRegistration.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.VisualStudio.ProjectSystem.VS;
-
-[assembly: ProjectTypeRegistration(
+﻿[assembly: Microsoft.VisualStudio.ProjectSystem.VS.ProjectTypeRegistration(
     projectTypeGuid: "068c306c-5a6f-4be1-bac6-b2cdec4f3b63",
-    displayName: ".",
-    displayProjectFileExtensions: ".",
+    displayName: "SystemTest Package",
+    displayProjectFileExtensions: "SystemTest Package project files (*.csproj);*.csproj",
     defaultProjectExtension: "csproj",
-    language: "SystemTestPackage",
-    PossibleProjectExtensions = "csproj"
-)]
+    language: "SystemTestPackage")]
